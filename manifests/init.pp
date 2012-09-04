ef713bb98bcb59c7cdf9935f8fe541d936a9ab7f
Alban Peignier <alban@tryphon.eu>
2012-09-04 17:41:45 +0200

    Initial import

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+class memcached {
+  package { memcached: }
+
+  include memcached::munin
+}
+
+class memcached::munin {
+  munin::plugin { [memcached_multi_bytes, memcached_multi_commands, memcached_multi_conns, memcached_multi_evictions, memcached_multi_items, memcached_multi_memory]: 
+    source => "puppet:///memcached/memcached_multi_"
+  }  
+}
