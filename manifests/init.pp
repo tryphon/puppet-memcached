a1d9e0edb716772349c8ed30a254b683f414c942
Alban Peignier <alban@tryphon.eu>
2012-09-04 18:10:38 +0200

    Use 'normal' memcached munin plugin

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@ class memcached {
 
 class memcached::munin {
   munin::plugin { [memcached_multi_bytes, memcached_multi_commands, memcached_multi_conns, memcached_multi_evictions, memcached_multi_items, memcached_multi_memory]: 
-    source => "puppet:///memcached/memcached_multi_"
+    source => "puppet:///memcached/memcached_"
   }  
 }
